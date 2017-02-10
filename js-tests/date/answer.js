2f91dfa35ce785ec79eb30218ca104b37bb9184c
Maks Ushakov <ush.maksim@gmail.com>
2017-02-10 13:38:09 +0200

    Add answer for formatDate question

--- js-tests/date/answer.js
+++ js-tests/date/answer.js
@@ -1,3 +1,9 @@
 function formatDate(userDate) {
   // format from M/D/YYYY to YYYYMMDD
+	var tmp = userDate.split('/');
+	var result = tmp[2];
+	tmp[0] = 0 + tmp[0];
+	tmp[1] = 0 + tmp[1];
+	result += tmp[0].slice(-2) + tmp[1].slice(-2);
+	return result;
 }
