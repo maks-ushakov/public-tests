99faa53269cd8f3a8bd7454afd48f72302618ff5
Maks Ushakov <ush.maksim@gmail.com>
2017-02-11 11:48:07 +0200

    Extend test cases

--- js-tests/ensure/test.js
+++ js-tests/ensure/test.js
@@ -1,10 +1,33 @@
 describe('Ensure', function () {
-	it('return True if value = True', function () {
-		expect(ensure(true)).to.be.true;
+	describe('return boolean arguments', function () {
+
+		it('return True if value = True', function () {
+			expect(ensure(true)).to.be.true;
+		});
+
+		it('return False if value = False', function () {
+			expect(ensure(false)).to.be.false;
+		});
 	});
 
-	it('return False if value = False', function () {
-		expect(ensure(false)).to.be.false;
+
+	describe('return given value', function () {
+		var valuesArr = [ 
+			1, // Number type
+			NaN, 
+			Infinity,
+			null, // null type
+			'String', // string type
+			true, // boolean type
+			{x: 1, y: 0}, // object type
+			[1, 2, 3] // Array type
+		];
+		for(var i = 0; i < valuesArr.length; i++) {
+			var item = valuesArr[i];
+			it('should return ' + typeof(item) + ' type, value = ' + item, function () {
+				expect(ensure(item)).to.equal(item);
+			});
+		}
 	});
 
 	describe('Errors', function () {
@@ -13,12 +36,16 @@ describe('Ensure', function () {
 		});
 
 		it('Throw Error if no arguments', function () {
-			assert.throws(function() { ensure(); }, Error);
+			expect(function() { ensure(); }).throw( Error );
 		});
 
 		it('Throw Error for undefined value', function () {
 			var tmp;
-			assert.throws(function () { ensure(tmp); }, Error );
+			expect(function () { ensure(tmp); }).throw( Error );
+		});
+
+		it('Do not throw Error if any difined arguments present', function () {
+			expect(function() { ensure(true); }).not.throw( Error);
 		});
 	});
 
