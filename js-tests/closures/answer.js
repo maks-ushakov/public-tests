12a7cdfc8ebd9f815f39bfc35f1dbd87dabfebb8
Maks Ushakov <ush.maksim@gmail.com>
2017-02-11 23:38:16 +0200

    Add answer to the Closure Question

--- js-tests/closures/answer.js
+++ js-tests/closures/answer.js
@@ -1,8 +1,12 @@
 function registerHandlers() {
   var as = document.getElementsByTagName('a');
   for (var i = 0; i < as.length; i++) {
+	  //Since the variable is changed by the time the event click come,
+	  //let's keep it as a property
+	  as[i].index = i;
     as[i].onclick = function() {
-      alert(i);
+		// Then use it
+      alert(this.index);
       return false;
     }
   }
