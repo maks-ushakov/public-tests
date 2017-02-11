a75a029b97266756b050b39200bea95d8bc43848
Maks Ushakov <ush.maksim@gmail.com>
2017-02-12 00:10:45 +0200

    Add answer to the Loop question

--- js-tests/loop/answer.js
+++ js-tests/loop/answer.js
@@ -1,8 +1,9 @@
 function appendChildren() {
   var allDivs = document.getElementsByTagName("div");
 
-  for (var i = 0; i < allDivs.length; i++) {
-    var newDiv = document.createElement("div");
+  // let cache initial array size
+  for (var i = 0, size = allDivs.length; i < size; i++) {
+	var newDiv = document.createElement("div");
     decorateDiv(newDiv);
     allDivs[i].appendChild(newDiv);
   }
