e0bdc25d4fa69cee91dd399487f80ad110741798
Maks Ushakov <ush.maksim@gmail.com>
2017-02-12 17:12:19 +0200

    Fix typos

--- js-tests/closures/answer.js
+++ js-tests/closures/answer.js
@@ -1,13 +1,13 @@
 function registerHandlers() {
-  var as = document.getElementsByTagName('a');
-  for (var i = 0; i < as.length; i++) {
-	  //Since the variable is changed by the time the event click come,
-	  //let's keep it as a property
-	  as[i].index = i;
-    as[i].onclick = function() {
-		// Then use it
-      alert(this.index);
-      return false;
-    }
-  }
+	var as = document.getElementsByTagName('a');
+		for (var i = 0; i < as.length; i++) {
+			//Since the variable is changed by the time the event click come,
+			//let's keep it as a property
+			as[i].index = i;
+			as[i].onclick = function() {
+			// Then use added property 
+			alert(this.index);
+			return false;
+		}
+	}
 }
